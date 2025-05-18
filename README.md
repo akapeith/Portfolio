c88e6744ebac1255d7134bd4fe0736726ecf3686
Khodor Wahab <akapeith@gmail.com>
2025-05-18 11:18:55 +0300

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# khodor.dev - Personal Portfolio and Projects
+
+This is my personal portfolio website built with React, showcasing my skills as a full-stack developer specializing in responsive web and mobile applications. The project features a clean, modern design with smooth navigation and responsive layouts to ensure a seamless experience across devices. It demonstrates my ability to build interactive user interfaces and implement best practices in front-end and back-end development. Feel free to explore the source code, learn about my projects, and get in touch through the contact section.
---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+import { LoadingScreen } from "./assets/Components/LoadingScreen"
+import { Navbar } from "./assets/Components/Navbar"
+import { MobileMenu } from "./assets/Components/MobileMenu"
+import { Home } from "./assets/Components/Sections/Home"
+import "../src/index.css"
+import { useState } from "react"
+import "../src/App.css"
+function App() {
+  const [isLoaded, setIsLoaded] = useState(false);
+  const [menuOpen, setMenuOpen] = useState(false);
+  return (
+    <>
+    {!isLoaded && <LoadingScreen onComplete={() =>setIsLoaded(true)}/>}
+      <div
+      className={`min-h-screen transition-opacity duration-700 ${
+        isLoaded ? "opacity-100" : "opacity-0"} bg-black text-gray-100`}
+      >
+
+      <Navbar menuOpen={menuOpen} setMenuOpen={setMenuOpen}/>
+      <MobileMenu menuOpen={menuOpen} setMenuOpen={setMenuOpen}/>
+      <Home/>
+    </div>
+    </>
+  )
+}
+
+export default App
